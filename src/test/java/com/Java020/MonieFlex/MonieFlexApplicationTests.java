d0f74dfc2abd9e2ba57a007c1bc35958a4a41651
abupraise <praiseabu2000@gmail.com>
2024-06-13 06:29:35 +0100

    Add sample test

--- src/test/java/com/Java020/MonieFlex/MonieFlexApplicationTests.java
+++ src/test/java/com/Java020/MonieFlex/MonieFlexApplicationTests.java
@@ -3,11 +3,19 @@ package com.Java020.MonieFlex;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class MonieFlexApplicationTests {
+public class MonieFlexApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void sampleTest() {
+		int expected = 42;
+		int actual = 42;
+		assertEquals(expected, actual);
+	}
 }
